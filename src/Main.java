8bd8e07569c19a1bd65983be99bfd5e0cda0d864
tashik <shahriar.kabir.tashik@gmail.com>
2023-11-27 00:24:48 -0600

    Leetcode 1 added

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {3,3};
+        Leetcode1 leetcode1 = new Leetcode1();
+        System.out.println(Arrays.toString(leetcode1.twoSum(array, 6)));
+    }
+}
